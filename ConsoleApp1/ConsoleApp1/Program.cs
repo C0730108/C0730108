f60d8ab9762dc4bf533a922c585e1011208421b9
C0730108 <jashandeepkaur717@gmail.com>
2019-01-31 14:29:30 -0500

    created a node called village create a link list called countryside

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+
+        }
+    }
+
+    class Village
+    {
+
+
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
+        {
+            Village.numberOfVillages++;
+        }
+        class Countryside
+        {
+            public Village Maple;
+            public Village Toronto;
+            public Village Ajax;
+            public void MapDomainInitializer()
+
+            {
+                Maple = new Village();
+                Maple.VillageName = "Maple";
+                Maple.previousVillage = null;
+                Maple.nextVillage = Toronto;
+                Toronto = new Village();
+                Toronto.previousVillage = Maple;
+                Toronto.VillageName = " Toronto";
+                Toronto.nextVillage = Ajax;
+                Ajax = new Village();
+                Ajax.previousVillage = Toronto;
+                Ajax.VillageName = " Maple";
+                Ajax.nextVillage = Ajax;
+                Ajax.isAstrildeHere = true;
+            }
+
+            public void LookForAstrilde()
+            {
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
